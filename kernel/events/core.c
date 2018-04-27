7a736f61216f28ae9051761db6ce2c7a5a02e37d
SamarV-121 <samarvispute121@gmail.com>
2018-04-27 14:36:25 +0530

    nicklaus: update to MMI-NMA26.42-146
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7671,6 +7671,29 @@ SYSCALL_DEFINE5(perf_event_open,
 
 	if (move_group) {
 		gctx = __perf_event_ctx_lock_double(group_leader, ctx);
+/*<<<<<<< HEAD
+
+		/*
+		 * Check if we raced against another sys_perf_event_open() call
+		 * moving the software group underneath us.
+		 */
+	//	if (!(group_leader->group_flags & PERF_GROUP_SOFTWARE)) {
+			/*
+			 * If someone moved the group out from under us, check
+			 * if this new event wound up on the same ctx, if so
+			 * its the regular !move_group case, otherwise fail.
+			 */
+	//		if (gctx != ctx) {
+	//			err = -EINVAL;
+	//			goto err_locked;
+	//		} else {
+	//			perf_event_ctx_unlock(group_leader, gctx);
+	//			move_group = 0;
+	//		}
+	//	}
+
+		/*
+======= */
 
 		/*
 		 * Check if we raced against another sys_perf_event_open() call
